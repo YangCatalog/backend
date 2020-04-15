f23a40a1eaaaa0424fa24bb4510769c86998438f
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-04-15 09:10:42 +0200

    Add json import, copyright and documentation
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- parseAndPopulate/nullJsonEncoder.py
+++ parseAndPopulate/nullJsonEncoder.py
@@ -1,3 +1,29 @@
+# Copyright The IETF Trust 2020, All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This is for use with json.dump(s) option. It will dump json without
+null values of the json. use cls option - cls=NullJsonEncoder
+"""
+import json
+
+__author__ = "Miroslav Kovac"
+__copyright__ = "Copyright The IETF Trust 2020, All Rights Reserved"
+__license__ = "Apache License, Version 2.0"
+__email__ = "miroslav.kovac@pantheon.tech"
+
+
 class NullJsonEncoder(json.JSONEncoder):
 
     def encode(self, o):
