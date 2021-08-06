e85f5aad58c0cdf4d131f871b6d5a6e47bd03fe7
richardzilincikPantheon <richard.zilincik@pantheon.tech>
2021-08-06 14:39:10 +0200

    Fix mock

--- tests/test_api_internal.py
+++ tests/test_api_internal.py
@@ -34,7 +34,7 @@ class TestApiInternalClass(unittest.TestCase):
         self.resources_path = '{}/resources/'.format(os.path.dirname(os.path.abspath(__file__)))
         self.client = application.test_client()
 
-    @mock.patch.object(yc_gc.sender, 'send', mock.MagicMock(return_value=1))
+    @mock.patch('api.sender.Sender.send')
     def test_trigger_ietf_pull(self):
         auth.hash_password(lambda _: 'True')
         auth.get_password(lambda _: 'True')
@@ -46,7 +46,7 @@ class TestApiInternalClass(unittest.TestCase):
         self.assertIn('job-id', data)
         self.assertEqual(data['job-id'], 1)
 
-    @mock.patch.object(yc_gc.sender, 'send', mock.MagicMock(return_value=1))
+    @mock.patch('api.sender.Sender.send')
     def test_trigger_ietf_pull_not_admin(self):
         auth.hash_password(lambda _: 'True')
         auth.get_password(lambda _: 'True')
