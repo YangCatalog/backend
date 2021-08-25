YangCatalog/backend

commit 64a4ca24e8c853485b0c1c3e869a6be7c5544ab4

Fix module deletion for dependencies without revisions

parseAndPopulate/modulesComplicatedAlgorithms.py
@@ -72,7 +72,7 @@ class ModulesComplicatedAlgorithms:
         existing_modules = response.json().get('module', [])
         self.__existing_modules_dict = defaultdict(dict)
         self.__latest_revisions = {}
-        for module in existing_modules:
+        for module in existing_modules + all_modules.get('module', []):
             # Store latest revision of each module - used in resolving tree-type
             latest_revision = self.__latest_revisions.get(module['name'])
             if latest_revision is None:
@@ -91,6 +91,8 @@ class ModulesComplicatedAlgorithms:
         self.parse_semver()
         LOGGER.info('parsing dependents')
         self.parse_dependents()
+        LOGGER.info('setting dependency revisions')
+        self.set_dependency_revisions()
 
     def populate(self):
         new_modules = [revision for name in self.new_modules.values() for revision in name.values()]
@@ -719,6 +721,18 @@ class ModulesComplicatedAlgorithms:
         add_dependents(all_modules, both_dict)
         LOGGER.info('Adding existing modules as dependents')
         add_dependents(existing_modules, all_modules_dict)
+
+    def set_dependency_revisions(self):
+        for module in self.__all_modules.get('module', []):
+            updated = False
+            dependencies = module.get('dependencies', [])
+            for dependency in dependencies:
+                if 'revision' not in dependency:
+                    dependency['revision'] = self.__latest_revisions[dependency['name']]
+                    updated = True
+            if updated:
+                new_module = self.new_modules[module['name']]
+                new_module.setdefault(module['revision'], deepcopy(module))['dependencies'] = dependencies
 
     def __find_file(self, name: str, revision: str = '*'):
         yang_name = '{}.yang'.format(name)

api/receiver.py
@@ -34,6 +34,7 @@ __email__ = "miroslav.kovac@pantheon.tech"
 
 import argparse
 import errno
+import functools
 import json
 import logging
 import multiprocessing
@@ -51,7 +52,6 @@ import utility.log as log
 from parseAndPopulate.modulesComplicatedAlgorithms import \
     ModulesComplicatedAlgorithms
 from utility import messageFactory
-
 from utility.util import prepare_to_indexing, send_to_indexing2
 from utility.staticVariables import confd_headers, json_headers
 
@@ -417,12 +417,55 @@ class Receiver:
         reason = ''
         paths = []
         modules = json.loads(path_to_delete)['modules']
-        for mod in modules:
-            paths.append('{}/restconf/data/yang-catalog:catalog/modules/module={},{},{}'.format(
-                confd_url, mod['name'], mod['revision'], mod['organization']))
         all_mods = requests.get('{}search/modules'.format(self.__yangcatalog_api_prefix)).json()
 
-        for mod in modules:
+        def fix_dependencies(module):
+            all_modules = {'module': [module]}
+            confd_prefix = '{}://{}:{}'.format(self.__confd_protocol, self.__confd_ip, self.__confd_port)
+            direc = '/var/yang/tmp'
+            mca = ModulesComplicatedAlgorithms(self.__log_directory, self.__yangcatalog_api_prefix,
+                                               self.__confd_credentials, confd_prefix, self.__save_file_dir, direc,
+                                               all_modules, self.__yang_models, self.temp_dir)
+            mca.set_dependency_revisions()
+            mca.populate()
+
+        def module_in(name: str, revision: str, modules: list):
+            for module in modules:
+                if module['name'] == name and module['revision'] == revision:
+                    return True
+            return False
+
+        @functools.lru_cache(maxsize=None)
+        def can_delete(name: str, revision: str):
+            for existing_module in all_mods['module']:
+                for dep_type in ['dependencies', 'submodule']:
+                    try:
+                        is_dep = module_in(name, revision, existing_module.get(dep_type, []))
+                    except KeyError:
+                        # some of our modules don't have revisions specified in dependencies
+                        fix_dependencies(existing_module)
+                        is_dep = module_in(name, revision, existing_module.get(dep_type, []))
+                    if is_dep:
+                        if module_in(existing_module['name'], existing_module['revision'], modules):
+                            if can_delete(existing_module['name'], existing_module['revision']):
+                                continue
+                        else:
+                            self.LOGGER.error('{}@{} module has reference in another module\'s {}: {}@{}'
+                                                    .format(name, revision, dep_type,
+                                                            existing_module.get('name'), existing_module.get('revision')))
+                            return False
+            return True
+
+        modules_to_delete = []
+        for module in modules:
+            if can_delete(module.get('name'), module.get('revision')):
+                modules_to_delete.append(module)
+
+        for mod in modules_to_delete:
+            paths.append('{}/restconf/data/yang-catalog:catalog/modules/module={},{},{}'.format(
+                confd_url, mod['name'], mod['revision'], mod['organization']))
+
+        for mod in modules_to_delete:
             for existing_module in all_mods['module']:
                 if existing_module.get('dependents') is not None:
                     dependents = existing_module['dependents']

api/views/userSpecificModuleMaintenance/moduleMaintenance.py
@@ -19,7 +19,6 @@ __email__ = "miroslav.kovac@pantheon.tech"
 
 import base64
 import errno
-import functools
 import json
 import os
 import shutil
@@ -147,36 +146,7 @@ def delete_modules(name: str = '', revision: str = '', organization: str = ''):
     all_mods = requests.get('{}search/modules'.format(yc_gc.yangcatalog_api_prefix)).json()
     # Filter out unavailble modules
     input_modules = [x for x in input_modules if x not in unavailable_modules]
-
-    def module_in(name: str, revision: str, modules: list):
-        for module in modules:
-            if module['name'] == name and module['revision'] == revision:
-                return True
-        return False
-
-    @functools.lru_cache(maxsize=None)
-    def can_delete(name: str, revision: str):
-        for existing_module in all_mods['module']:
-            for dep_type in ['dependencies', 'submodule']:
-                if module_in(name, revision, existing_module.get(dep_type, [])):
-                    if module_in(existing_module['name'], existing_module['revision'], input_modules):
-                        if can_delete(existing_module['name'], existing_module['revision']):
-                            continue
-                    else:
-                        current_app.logger.error('{}@{} module has reference in another module\'s {}: {}@{}'
-                                                 .format(name, revision, dep_type,
-                                                         existing_module.get('name'), existing_module.get('revision')))
-                        return False
-        return True
-
-    modules_to_delete = {'modules': []}
-    for module in input_modules:
-        if can_delete(module.get('name'), module.get('revision')):
-            modules_to_delete['modules'].append(module)
-        else:
-            unavailable_modules.append(module)
-
-    path_to_delete = json.dumps(modules_to_delete)
+    path_to_delete = json.dumps({'modules':input_modules})
 
     arguments = [yc_gc.protocol, yc_gc.confd_ip, repr(yc_gc.confdPort), yc_gc.credentials[0],
                  yc_gc.credentials[1], path_to_delete, 'DELETE',

commit d8e1cfbc98c854e84325d99c43e5176c2cf5b42e

Fix bug introduced in 64a4ca2

Dockerfile
@@ -2,10 +2,12 @@ FROM python:3.9
 ARG YANG_ID
 ARG YANG_GID
 ARG CRON_MAIL_TO
+ARG YANGCATALOG_CONFIG_PATH
 
 ENV YANG_ID "$YANG_ID"
 ENV YANG_GID "$YANG_GID"
 ENV CRON_MAIL_TO "$CRON_MAIL_TO"
+ENV YANGCATALOG_CONFIG_PATH "$YANGCATALOG_CONFIG_PATH"
 ENV LANG=C.UTF-8 LC_ALL=C.UTF-8 PYTHONUNBUFFERED=1
 
 ENV VIRTUAL_ENV=/backend
@@ -29,6 +31,12 @@ RUN groupadd -g ${YANG_GID} -r yang \
   && virtualenv --system-site-packages $VIRTUAL_ENV \
   && mkdir -p /etc/yangcatalog
 
+WORKDIR $VIRTUAL_ENV
+RUN git clone https://github.com/slatedocs/slate.git
+WORKDIR $VIRTUAL_ENV/slate
+RUN bundle install
+RUN bundle exec middleman build --clean
+
 COPY ./backend $VIRTUAL_ENV
 ENV PYTHONPATH=$VIRTUAL_ENV/bin/python
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
@@ -47,7 +55,8 @@ COPY ./backend/crontab /etc/cron.d/yang-cron
 RUN mkdir /var/run/yang
 
 RUN chown yang:yang /etc/cron.d/yang-cron
-RUN sed -i "s|<MAIL_TO>|${CRON_MAIL_TO} |g" /etc/cron.d/yang-cron
+RUN sed -i "s|<MAIL_TO>|${CRON_MAIL_TO}|g" /etc/cron.d/yang-cron
+RUN sed -i "s|<YANGCATALOG_CONFIG_PATH>|${YANGCATALOG_CONFIG_PATH}|g" /etc/cron.d/yang-cron
 RUN chown -R yang:yang $VIRTUAL_ENV
 RUN chown -R yang:yang /var/run/yang
 
@@ -64,8 +73,6 @@ RUN chmod 644 /etc/logrotate.d/yangcatalog-rotate
 
 USER ${YANG_ID}:${YANG_GID}
 
-RUN git clone https://github.com/slatedocs/slate.git
-
 WORKDIR $VIRTUAL_ENV/slate
 
 RUN rm -rf source
@@ -77,15 +84,11 @@ WORKDIR $VIRTUAL_ENV
 RUN crontab /etc/cron.d/yang-cron
 
 USER root:root
-WORKDIR $VIRTUAL_ENV/slate
-RUN bundle install
-RUN bundle exec middleman build --clean
-WORKDIR $VIRTUAL_ENV
 RUN mkdir -p /usr/share/nginx/html/stats
 RUN cp -R $VIRTUAL_ENV/slate /usr/share/nginx/html
 RUN chown -R yang:yang /usr/share/nginx
 RUN ln -s /usr/share/nginx/html/stats/statistics.html /usr/share/nginx/html/statistics.html
 
-CMD chown -R yang:yang /var/run/yang && cron && service postfix start && service rsyslog start && /backend/bin/gunicorn api.wsgi:application -c gunicorn.conf.py
+CMD chown -R yang:yang /var/run/yang && cron && service postfix start && service rsyslog start && /backend/bin/gunicorn api.wsgi:application -c gunicorn.conf.py --preload
 
 EXPOSE 3031

parseAndPopulate/modulesComplicatedAlgorithms.py
@@ -72,14 +72,14 @@ class ModulesComplicatedAlgorithms:
         existing_modules = response.json().get('module', [])
         self.__existing_modules_dict = defaultdict(dict)
         self.__latest_revisions = {}
-        for module in existing_modules + all_modules.get('module', []):
+        for module in existing_modules + self.__all_modules.get('module', []):
             # Store latest revision of each module - used in resolving tree-type
             latest_revision = self.__latest_revisions.get(module['name'])
             if latest_revision is None:
                 self.__latest_revisions[module['name']] = module['revision']
             else:
                 self.__latest_revisions[module['name']] = max(module['revision'], latest_revision)
-
+        for module in existing_modules:
             self.__existing_modules_dict[module['name']][module['revision']] = module
 
     def parse_non_requests(self):
@@ -725,7 +725,7 @@ class ModulesComplicatedAlgorithms:
     def set_dependency_revisions(self):
         for module in self.__all_modules.get('module', []):
             updated = False
-            dependencies = module.get('dependencies', [])
+            dependencies = deepcopy(module.get('dependencies', []))
             for dependency in dependencies:
                 if 'revision' not in dependency:
                     dependency['revision'] = self.__latest_revisions[dependency['name']]