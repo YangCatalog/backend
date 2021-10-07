ff60a4ac4dcae566f7c87c8794793dcd75341967
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2021-10-07 09:44:58 +0200

    Bugfix: Remove unnecessary arguments from script calls
    - After confdService was added, some script arguments were removed but
    not removed from script/method calls
    
    This resolves #374
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- api/views/userSpecificModuleMaintenance/moduleMaintenance.py
+++ api/views/userSpecificModuleMaintenance/moduleMaintenance.py
@@ -48,6 +48,7 @@ class UserSpecificModuleMaintenance(Blueprint):
 
 bp = UserSpecificModuleMaintenance('userSpecificModuleMaintenance', __name__)
 
+
 @bp.before_request
 def set_config():
     global ac, db
@@ -91,6 +92,7 @@ def register_user():
     mf = MessageFactory()
     mf.send_new_user(username, email, motivation)
     return ({'info': 'User created successfully'}, 201)
+
 
 @bp.route('/modules/module/<name>,<revision>,<organization>', methods=['DELETE'])
 @bp.route('/modules', methods=['DELETE'])
@@ -136,14 +138,14 @@ def delete_modules(name: str = '', revision: str = '', organization: str = ''):
 
         if read['yang-catalog:module'][0].get('organization') != accessRigths and accessRigths != '/':
             abort(401, description='You do not have rights to delete modules with organization {}'
-                       .format(read['yang-catalog:module'][0].get('organization')))
+                  .format(read['yang-catalog:module'][0].get('organization')))
 
         if read['yang-catalog:module'][0].get('implementations') is not None:
             unavailable_modules.append(mod)
 
     # Filter out unavailble modules
     input_modules = [x for x in input_modules if x not in unavailable_modules]
-    path_to_delete = json.dumps({'modules':input_modules})
+    path_to_delete = json.dumps({'modules': input_modules})
 
     arguments = [ac.g_protocol_confd, ac.w_confd_ip, ac.w_confd_port, ac.s_confd_credentials[0],
                  ac.s_confd_credentials[1], path_to_delete, 'DELETE',
@@ -304,7 +306,7 @@ def add_modules():
                 repo[repo_url].clone()
             except GitCommandError as e:
                 abort(400, description='bad request - cound not clone the github repository. Please check owner,'
-                                        ' repository and path of the request - {}'.format(e.stderr))
+                      ' repository and path of the request - {}'.format(e.stderr))
 
         try:
             if 'branch' in sdo:
@@ -353,7 +355,7 @@ def add_modules():
                         os.path.abspath('{}/{}/{}.yang'.format(repo[repo_url].localdir,
                                                                '/'.join(sdo_path.split('/')[:-1]),
                                                                belongs_to))
-                        ).search('namespace')[0].arg
+                    ).search('namespace')[0].arg
                     for ns, org in NS_MAP.items():
                         if ns in namespace:
                             organization = org
@@ -486,7 +488,7 @@ def add_vendors():
                 repo[repo_url].clone()
             except GitCommandError as e:
                 abort(400, description='bad request - cound not clone the github repository. Please check owner,'
-                                              ' repository and path of the request - {}'.format(e.stderr))
+                      ' repository and path of the request - {}'.format(e.stderr))
         try:
             if 'branch' in capability:
                 branch = capability.get('branch')
@@ -600,7 +602,7 @@ def get_job(job_id):
     return {'info': {'job-id': job_id,
                      'result': result,
                      'reason': reason}
-                    }
+            }
 
 ### HELPER DEFINITIONS ###
 
@@ -622,6 +624,7 @@ def get_user_access_rights(username: str, is_vendor: bool = False):
         app.logger.error('Cannot connect to database. MySQL error: {}'.format(err))
 
     return accessRigths
+
 
 def get_mod_confd(name: str, revision: str, organization: str):
     mod_key = '{},{},{}'.format(name, revision, organization)
