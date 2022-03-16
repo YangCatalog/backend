490e300321459e8e3eaf9a9c6929dc63bd20e93d
richardzilincikPantheon <richard.zilincik@pantheon.tech>
2022-03-16 09:02:09 +0100

    Adjust prepare_environment.sh for local deployments

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,13 +1,10 @@
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-export BACKEND=$(pwd)
-export VAR=/var/yang
-export TMP_DIR=$VAR/tmp
-export SAVE_FILE_DIR=$VAR/all_modules
-export YANG_MODELS_DIR=$VAR/nonietf/yangmodels/yang
+BACKEND=$(pwd)
+VAR=/var/yang
+TMP_DIR=$VAR/tmp
+SAVE_FILE_DIR=$VAR/all_modules
+YANG_MODELS_DIR=$VAR/nonietf/yangmodels/yang
 
 # Prepare /var/yang directory structure
-sudo mkdir -p $VAR
-sudo chown -R $(whoami):$(whoami) $VAR
 mkdir -p $TMP_DIR
 mkdir -p $VAR/ytrees
 mkdir -p $SAVE_FILE_DIR
@@ -16,10 +13,13 @@ mkdir -p $YANG_MODELS_DIR
 # Create files in tmp dir
 touch $TMP_DIR/normal.json $TMP_DIR/prepare.json
 
-# Clone YangModels/yang repository
-git clone --depth 1 https://github.com/YangModels/yang.git $YANG_MODELS_DIR
-cd $YANG_MODELS_DIR
-git submodule update --init vendor/huawei
+# Clone YangModels/yang repository if it's not already present
+if [[ ! -d $YANG_MODELS_DIR/vendor/huawei ]]
+then
+    git clone --depth 1 https://github.com/YangModels/yang.git $YANG_MODELS_DIR
+    cd $YANG_MODELS_DIR
+    git submodule update --init vendor/huawei
+fi
 
 # Prepare files and directory structure for test_groupings.py
 mkdir -p $TMP_DIR/groupings-tests/YangModels/yang/standard/ietf/RFC
@@ -42,14 +42,16 @@ cp $YANG_MODELS_DIR/vendor/cisco/xe/16101/capability-asr1k.xml $TEST_REPO/vendor
 cp $YANG_MODELS_DIR/standard/ietf/RFC/ietf-yang-types@2013-07-15.yang $TEST_REPO/standard/ietf/RFC
 
 # Prepare Huawei directory for ietf-yang-lib based tests
-rm -rf $YANG_MODELS_DIR/vendor/huawei/network-router/8.20.0/atn980b
-rm -rf $YANG_MODELS_DIR/vendor/huawei/network-router/8.20.0/ne40e-x8x16
-rm -rf $YANG_MODELS_DIR/vendor/huawei/network-router/8.20.10
-rm -rf $YANG_MODELS_DIR/vendor/huawei/network-router/8.21.0
-export YANG_MODELS_HUAWEI_DIR=$YANG_MODELS_DIR/vendor/huawei/network-router/8.20.0/ne5000e
+YANG_MODELS_HUAWEI_DIR=$YANG_MODELS_DIR/vendor/huawei/network-router/8.20.0/ne5000e
 mkdir -p $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e
 cp $YANG_MODELS_HUAWEI_DIR/ietf*.yang $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e/
 cp $YANG_MODELS_HUAWEI_DIR/huawei-aaa*.yang $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e/
+
+for dependency in huawei-pub-type huawei-hwtacacs huawei-extension huawei-network-instance huawei-radius-client
+do
+    cp $YANG_MODELS_HUAWEI_DIR/$dependency.yang $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e/
+done
+
 cp $BACKEND/tests/resources/platform-metadata.json $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e/
 cp $BACKEND/tests/resources/ietf-yang-library.xml $TEST_REPO/vendor/huawei/network-router/8.20.0/ne5000e/
 
@@ -57,7 +59,6 @@ cp $BACKEND/tests/resources/ietf-yang-library.xml $TEST_REPO/vendor/huawei/netwo
 mkdir -p $TMP_DIR/util-tests
 cp $YANG_MODELS_DIR/standard/ietf/RFC/ietf-yang-types.yang $TMP_DIR/util-tests/
 cp $YANG_MODELS_DIR/standard/ietf/RFC/ietf-yang-types@2010-09-24.yang $TMP_DIR/util-tests/
-cd $BACKEND
 
 # Prepare directory structure need for test_resolveExpiration.py
 # Copy all RFC modules into /var/yang/all_modules directory
