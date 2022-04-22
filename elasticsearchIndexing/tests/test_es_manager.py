69e102df76ebca335ff0443f17aa22a2ad8ac63b
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2022-04-22 14:46:18 +0200

    Multiple TestCase classes created
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- elasticsearchIndexing/tests/test_es_manager.py
+++ elasticsearchIndexing/tests/test_es_manager.py
@@ -25,18 +25,14 @@ from unittest import mock
 from elasticsearch import Elasticsearch
 from elasticsearchIndexing.es_manager import ESManager
 from elasticsearchIndexing.models.es_indices import ESIndices
+from elasticsearchIndexing.models.keywords_names import KeywordsNames
 from utility.create_config import create_config
 
 
-class MockESManager(ESManager):
-    def __init__(self) -> None:
-        self.es = mock.MagicMock()
-
-
-class TestESManagerWithoutIndexClass(unittest.TestCase):
+class TestESManagerClass(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
-        super(TestESManagerWithoutIndexClass, self).__init__(*args, **kwargs)
+        super(TestESManagerClass, self).__init__(*args, **kwargs)
         config = create_config()
         es_host_config = {
             'host': config.get('DB-Section', 'es-host', fallback='localhost'),
@@ -44,10 +40,26 @@ class TestESManagerWithoutIndexClass(unittest.TestCase):
         }
         self.es = Elasticsearch(hosts=[es_host_config])
         self.test_index = ESIndices.TEST
+        self.resources_path = os.path.join(os.environ['BACKEND'], 'elasticsearchIndexing/tests/resources')
+        self.test_data = self._load_test_data()
+        self.es_manager = ESManager()
 
     def setUp(self):
-        self.es_manager = ESManager()
-        self.es.indices.delete(index=ESIndices.TEST.value, ignore=[400, 404])
+        self.es.indices.delete(index=self.test_index.value, ignore=[400, 404])
+
+    def tearDown(self) -> None:
+        self.es.indices.delete(index=self.test_index.value, ignore=[400, 404])
+
+    def _load_test_data(self):
+        data_file_path = os.path.join(self.resources_path, 'es_test_data.json')
+        test_data = {}
+        with open(data_file_path, 'r') as reader:
+            test_data = json.load(reader)
+
+        return test_data
+
+
+class TestESManagerWithoutIndexClass(TestESManagerClass):
 
     def test_create_index(self):
         create_result = self.es_manager.create_index(self.test_index)
@@ -57,21 +69,16 @@ class TestESManagerWithoutIndexClass(unittest.TestCase):
         self.assertTrue(create_result['acknowledged'])
         self.assertEqual(create_result['index'], self.test_index.value)
 
+    def test_index_exists(self):
+        index_exists = self.es_manager.index_exists(self.test_index)
 
-class TestESManagerWithIndexClass(unittest.TestCase):
+        self.assertFalse(index_exists)
 
-    def __init__(self, *args, **kwargs):
-        super(TestESManagerWithIndexClass, self).__init__(*args, **kwargs)
-        config = create_config()
-        es_host_config = {
-            'host': config.get('DB-Section', 'es-host', fallback='localhost'),
-            'port': config.get('DB-Section', 'es-port', fallback='9200')
-        }
-        self.es = Elasticsearch(hosts=[es_host_config])
-        self.test_index = ESIndices.TEST
+
+class TestESManagerWithIndexClass(TestESManagerClass):
 
     def setUp(self):
-        self.es_manager = ESManager()
+        super(TestESManagerWithIndexClass, self).setUp()
         index_json_name = f'initialize_{self.test_index.value}_index.json'
         index_json_path = os.path.join(os.environ['BACKEND'], 'elasticsearchIndexing/json/', index_json_name)
         with open(index_json_path, encoding='utf-8') as reader:
@@ -86,6 +93,33 @@ class TestESManagerWithIndexClass(unittest.TestCase):
         self.assertEqual(create_result['status'], 400)
         self.assertEqual(create_result['error']['index'], self.test_index.value)
         self.assertEqual(create_result['error']['type'], 'resource_already_exists_exception')
+
+    def test_index_exists(self):
+        index_exists = self.es_manager.index_exists(self.test_index)
+
+        self.assertTrue(index_exists)
+
+
+class TestESManagerAutocompleteClass(TestESManagerClass):
+
+    def setUp(self):
+        super(TestESManagerAutocompleteClass, self).setUp()
+        index_json_name = f'initialize_{self.test_index.value}_index.json'
+        index_json_path = os.path.join(os.environ['BACKEND'], 'elasticsearchIndexing/json/', index_json_name)
+        with open(index_json_path, encoding='utf-8') as reader:
+            index_config = json.load(reader)
+        self.es.indices.create(index=self.test_index.value, body=index_config, ignore=400)
+
+        autocomplete_modules = self.test_data['autocomplete_modules']
+        for module in autocomplete_modules:
+            self.es.index(index=self.test_index.value, body=module, refresh='true')
+
+    def test_autocomplete(self):
+        results = self.es_manager.autocomplete(self.test_index, KeywordsNames.NAME, 'ietf-')
+
+        self.assertNotEqual(results, [])
+        for result in results:
+            self.assertIn('ietf-', result)
 
 
 if __name__ == '__main__':
