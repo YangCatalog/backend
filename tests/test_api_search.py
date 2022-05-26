6e3c7066aa95f9e4e005f65ab9123e6d7ba9b73c
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2022-05-26 08:55:35 +0200

    Title added to the Bootstrap HTML pages (#506)
    * Title added to the Bootstrap HTML pages
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>
    
    * Unit tests adjustments
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- tests/test_api_search.py
+++ tests/test_api_search.py
@@ -25,8 +25,8 @@ from unittest import mock
 
 import api.views.ycSearch.ycSearch as search_bp
 from api.yangCatalogApi import app
+from lxml import html as HT
 from markupsafe import escape
-from lxml import etree as ET
 from werkzeug.exceptions import BadRequest, NotFound
 
 ac = app.config
@@ -381,7 +381,7 @@ class TestApiSearchClass(unittest.TestCase):
         path = 'api/services/file1={}@{}/check-update-from/file2={}@{}'.format(file1, revision1, file2, revision2)
         result = self.client.get(path)
 
-        response_text = ET.fromstring(result.data).find('body').find('pre').text # pyright: ignore
+        response_text = HT.fromstring(result.data).find('body').find('pre').text  # pyright: ignore
 
         self.assertEqual(desired_output, response_text)
 
@@ -966,7 +966,7 @@ class TestApiSearchClass(unittest.TestCase):
 
         result = self.client.get('api/services/tree/{}@{}.yang'.format(filename, revision))
         data = result.data.decode()
-        response_text = ET.fromstring(data).find('body').find('pre').text # pyright: ignore
+        response_text = HT.fromstring(data).find('body').find('div').find('pre').text  # pyright: ignore
 
         with open(os.path.join(self.resources_path, 'yang-tree.txt'), 'r') as f:
             yang_tree_data = f.readlines()
