c74c73c15079cf8001ae831c5ffc1645effa4130
richardzilincikPantheon <85497288+richardzilincikPantheon@users.noreply.github.com>
2022-08-16 13:23:19 +0200

    test: simplify test data for dumper.py (#560)

--- tests/test_dumper.py
+++ tests/test_dumper.py
@@ -20,10 +20,11 @@ __email__ = 'slavomir.mazur@pantheon.tech'
 import json
 import os
 import unittest
+import typing as t
 
+import parseAndPopulate.dumper as du
 from api.globalConfig import yc_gc
 from parseAndPopulate.dir_paths import DirPaths
-from parseAndPopulate.dumper import Dumper
 from parseAndPopulate.models.schema_parts import SchemaParts
 from parseAndPopulate.modules import SdoModule, VendorModule
 
@@ -36,17 +37,13 @@ class TestDumperClass(unittest.TestCase):
         # Declare variables
         self.schema_parts = SchemaParts(repo_owner='YangModels', repo_name='yang', commit_hash='master')
         self.prepare_output_filename = 'prepare'
-        self.sdo_module_filename = 'ietf-yang-types@2013-07-15.yang'
-        self.sdo_module_name = 'ietf-yang-types'
-        self.hello_message_filename = 'capabilities-ncs5k.xml'
-        self.platform_name = 'ncs5k'
-        self.resources_path = os.path.join(os.environ['BACKEND'], 'tests/resources')
-        self.test_private_dir = os.path.join(self.resources_path, 'html/private')
+        self.platform_name = 'test-platform'
+        self.resources_path = os.path.join(os.environ['BACKEND'], 'tests/resources/dumper')
         self.dir_paths: DirPaths = {
             'cache': '',
             'json': '',
             'log': yc_gc.logs_dir,
-            'private': self.test_private_dir,
+            'private': '',
             'result': yc_gc.result_dir,
             'save': yc_gc.save_file_dir,
             'yang_models': yc_gc.yang_models
@@ -61,19 +58,15 @@ class TestDumperClass(unittest.TestCase):
         Dumper object is initialized and key of one Modules object is added to 'yang_modules' dictionary.
         Created key is then retreived from 'yang_modules' dictionary and compared with desired format of key.
         """
-        desired_key = 'ietf-yang-types@2013-07-15/ietf'
+        key = 'sdo-module@2022-08-05/ietf'
 
         yang = self.declare_sdo_module()
 
-        dumper = Dumper(yc_gc.logs_dir, self.prepare_output_filename)
+        dumper = du.Dumper(yc_gc.logs_dir, self.prepare_output_filename)
         dumper.add_module(yang)
 
-        created_key = list(dumper.yang_modules.keys())[0]
+        self.assertEqual(list(dumper.yang_modules.keys()), [key])
 
-        self.assertEqual(created_key, desired_key)
-        self.assertIn(desired_key, dumper.yang_modules)
-
-    @unittest.skip('https://github.com/YangCatalog/backend/issues/543')
     def test_dumper_dump_modules(self):
         """
         Dumper object is created and one SDO module is added.
@@ -82,36 +75,22 @@ class TestDumperClass(unittest.TestCase):
         """
         yang = self.declare_sdo_module()
 
-        dumper = Dumper(yc_gc.logs_dir, self.prepare_output_filename)
+        dumper = du.Dumper(yc_gc.logs_dir, self.prepare_output_filename)
         dumper.add_module(yang)
         dumper.dump_modules(yc_gc.temp_dir)
 
         # Load desired module data from .json file
-        with open('{}/parseAndPopulate_tests_data.json'.format(self.resources_path), 'r') as f:
+        with open(self.resource('test_data.json'), 'r') as f:
             file_content = json.load(f)
-        desired_module_data = file_content['dumped_module']['module'][0]
+        desired_module_data = file_content['dumped_module']['module']
 
         # Load module data from dumped prepare.json file
         with open('{}/{}.json'.format(yc_gc.temp_dir, self.prepare_output_filename), 'r') as f:
             file_content = json.load(f)
-        dumped_module_data = file_content['module'][0]
+        dumped_module_data = file_content['module']
 
         # Compare properties/keys of desired and dumped module data objects
-        for key in desired_module_data:
-            self.assertIn(key, dumped_module_data, desired_module_data[key])
-            if key == 'yang-tree':
-                # Compare only URL suffix (exclude domain)
-                desired_tree_suffix = '/api{}'.format(desired_module_data[key].split('/api')[1])
-                dumped_tree_suffix = '/api{}'.format(dumped_module_data[key].split('/api')[1])
-                self.assertEqual(desired_tree_suffix, dumped_tree_suffix)
-            elif key == 'compilation-result':
-                if dumped_module_data[key] != '' and desired_module_data[key] != '':
-                    # Compare only URL suffix (exclude domain)
-                    desired_compilation_result = '/results{}'.format(desired_module_data[key].split('/results')[1])
-                    dumped_compilation_result = '/results{}'.format(dumped_module_data[key].split('/results')[1])
-                    self.assertEqual(desired_compilation_result, dumped_compilation_result)
-            else:
-                self.assertEqual(dumped_module_data[key], desired_module_data[key])
+        self.compare_module_data(dumped_module_data, desired_module_data)
 
     def test_dumper_dump_vendors(self):
         """
@@ -119,79 +98,38 @@ class TestDumperClass(unittest.TestCase):
         It is necessary that this module has filled information about the implementation.
         This can be achieved by calling add_vendor_information() method.
         Vendor data are then dumped into normal.json file using dump_vendors() method.
-        Content of dumped normal.json file is then compared with desired content loaded from parseAndPopulate_tests_data.json file.
+        Content of dumped normal.json file is then compared with desired content loaded from test_data.json file.
         """
-        # Modules object
-        directory = os.path.join(yc_gc.temp_dir, 'test/YangModels/yang/vendor/cisco/xr/701')
-        platform_data, netconf_version, netconf_capabilities = self.get_platform_data(directory)
+        platform_data = [{
+            'software-flavor': 'test-flavor',
+            'platform': 'test-platform',
+            'os-version': 'test-version',
+            'software-version': 'test-version',
+            'feature-set': 'ALL',
+            'vendor': 'cisco',
+            'os': 'test-os'
+        }]
+        netconf_version = ['urn:ietf:params:xml:ns:netconf:base:1.0']
+        netconf_capabilities = ['urn:ietf:params:netconf:capability:test-capability:1.0']
+        # Modules object
         yang = self.declare_vendor_module()
         yang.add_vendor_information(platform_data, 'implement', netconf_capabilities, netconf_version)
         # Dumper object
-        dumper = Dumper(yc_gc.logs_dir, self.prepare_output_filename)
+        dumper = du.Dumper(yc_gc.logs_dir, self.prepare_output_filename)
         dumper.add_module(yang)
         dumper.dump_vendors(yc_gc.temp_dir)
 
         # Load desired module data from .json file
-        with open(os.path.join(self.resources_path, 'parseAndPopulate_tests_data.json'), 'r') as f:
+        with open(self.resource('test_data.json'), 'r') as f:
             file_content = json.load(f)
-        desired_vendor_data = file_content.get('dumped_vendor_data', {})
+        desired_vendor_data = file_content['dumped_vendor_data']['vendors']['vendor']
 
         # Load vendor module data from normal.json file
         os.path.join(yc_gc.temp_dir, 'normal.json')
         with open(os.path.join(yc_gc.temp_dir, 'normal.json'), 'r') as f:
-            dumped_vendor_data = json.load(f)
+            dumped_vendor_data = json.load(f).get('vendors', {}).get('vendor', [])
 
-        self.assertEqual(desired_vendor_data, dumped_vendor_data)
-
-    def test_dumper_get_dependencies_none(self):
-        """
-        Set value of dependencies property to None, to test if __get_dependencies() method
-        correctly set value.
-        If value is set to None, it should not be dumped into normal.json file.
-        """
-        yang = self.declare_sdo_module()
-
-        # Clear dependencies property to test functionality of __get_dependencies() method
-        yang.dependencies = []
-
-        dumper = Dumper(yc_gc.logs_dir, self.prepare_output_filename)
-        dumper.add_module(yang)
-        dumper.dump_modules(yc_gc.temp_dir)
-
-        # Load module data from dumped .json file
-        with open('{}/{}.json'.format(yc_gc.temp_dir, self.prepare_output_filename), 'r') as f:
-            file_content = json.load(f)
-        dumped_module_data = file_content['module'][0]
-
-        # Since dependencies property has value None, it should not be present in dumped module
-        self.assertNotIn('dependencies', dumped_module_data)
-
-    def test_dumper_get_deviations_none(self):
-        """
-        Set value of deviations property to None, to test if __get_deviations() method
-        correctly set value.
-        If value is set to None, it should not be dumped into .json file.
-        """
-        directory = os.path.join(yc_gc.temp_dir, 'test/YangModels/yang/vendor/cisco/xr/701')
-        platform_data, netconf_version, netconf_capabilities = self.get_platform_data(directory)
-        yang = self.declare_vendor_module()
-        yang.add_vendor_information(platform_data, 'implement', netconf_capabilities, netconf_version)
-
-        # Clear deviations property to test functionality of __get_deviations() method
-        for implementation in yang.implementations:
-            implementation.deviations = []
-
-        # Dumper object
-        dumper = Dumper(yc_gc.logs_dir, self.prepare_output_filename)
-        dumper.add_module(yang)
-        dumper.dump_vendors(yc_gc.temp_dir)
-
-        # Load vendor module data from normal.json file
-        with open(os.path.join(yc_gc.temp_dir, 'normal.json'), 'r') as f:
-            dumped_vendor_data = json.load(f)
-
-        # Since deviations property has value None, it should not be present in dumped module
-        self.assertNotIn('deviations', dumped_vendor_data)
+        self.compare_vendor_data(desired_vendor_data, dumped_vendor_data)
 
     ##########################
     ### HELPER DEFINITIONS ###
@@ -201,12 +139,12 @@ class TestDumperClass(unittest.TestCase):
         """
         Initialize Modules object for SDO (ietf) module.
 
-        :returns:           Created instance of Modules object of SDO (ietf) module
-        :rtype: Modules
+        Arguments:
+            :return     (SdoModule) Created instance of Modules object of SDO (ietf) module
         """
-        path_to_yang = os.path.join(yc_gc.temp_dir, 'test/YangModels/yang/standard/ietf/RFC', self.sdo_module_filename)
+        path_to_yang = self.resource('sdo-module.yang')
 
-        yang = SdoModule(self.sdo_module_name, path_to_yang, {}, self.dir_paths, {})
+        yang = SdoModule('sdo-module', path_to_yang, {}, self.dir_paths, {})
 
         return yang
 
@@ -214,47 +152,130 @@ class TestDumperClass(unittest.TestCase):
         """
         Initialize Modules object for vendor (Cisco) module.
 
-        :returns:           Created instance of Modules object of vendor (cisco) module
-        :rtype: Modules
+        Arguments:
+            :return     (VendorModule) Created instance of Modules object of vendor (cisco) module
         """
-        vendor_data = 'ietf-netconf-acm&revision=2018-02-14&deviations=cisco-xr-ietf-netconf-acm-deviations'
+        vendor_data = 'sdo-module&revision=2022-08-05&deviations=vendor-sdo-module-deviations'
         module_name = vendor_data.split('&revision')[0]
-        module_path = \
-            os.path.join(yc_gc.temp_dir, 'test/YangModels/yang/vendor/cisco/xr/701', '{}.yang'.format(module_name))
+        path_to_yang = self.resource('sdo-module.yang')
 
-        yang = VendorModule(module_name, module_path, {}, self.dir_paths, {}, data=vendor_data)
+        yang = VendorModule(module_name, path_to_yang, {}, self.dir_paths, {}, data=vendor_data)
 
         return yang
 
-    def get_platform_data(self, directory: str):
-        """
-        Load content of platform-metadata.json file and parse data of selected platform.
+    def compare_module_data(self, desired_module_data, dumped_module_data):
+        for desired_module in desired_module_data:
+            name = desired_module.get('name')
+            revision = desired_module.get('revision')
+            for dumped_module in dumped_module_data:
+                if name == dumped_module.get('name'):
+                    fail_message = 'mismatch in {}'.format(name)
+                    fail_message = '{} ' + fail_message
+                    # Compare properties/keys of desired and dumped module data objects
+                    for key in desired_module:
+                        if name == 'ietf-yang-library':
+                            # We have multiple slightly different versions of ietf-yang-library
+                            # marked with the same revision
+                            if key in ('description', 'contact'):
+                                continue
+                        if key == 'yang-tree':
+                            # Compare only URL suffix (exclude domain)
+                            desired_tree_suffix = '/api{}'.format(desired_module[key].split('/api')[1])
+                            dumped_tree_suffix = '/api{}'.format(dumped_module[key].split('/api')[1])
+                            self.assertEqual(desired_tree_suffix, dumped_tree_suffix,
+                                             fail_message.format('tree suffix'))
+                        elif key == 'compilation-result':
+                            if dumped_module[key] != '' and desired_module[key] != '':
+                                # Compare only URL suffix (exclude domain)
+                                desired_compilation_result = '/results{}'.format(desired_module[key].split('/results')[-1])
+                                dumped_compilation_result = '/results{}'.format(dumped_module[key].split('/results')[-1])
+                                self.assertEqual(desired_compilation_result, dumped_compilation_result,
+                                                 fail_message.format('compilation result'))
+                        else:
+                            if isinstance(desired_module[key], list):
+                                # submodules or dependencies
+                                for i in desired_module[key]:
+                                    for j in dumped_module[key]:
+                                        try:
+                                            j.pop('schema')
+                                        except KeyError:
+                                            pass
+                                    self.assertIn(i, dumped_module[key], fail_message.format(key))
+                            else:
+                                self.assertEqual(dumped_module[key], desired_module[key], fail_message.format(key))
+                    break
+            else:
+                self.assertTrue(False, '{}@{} not found in dumped data'.format(name, revision))
 
-        :param xml_path         (str) Absolute path of selected .xml file
-        """
-        platform_data = []
-        netconf_version = []
-        netconf_capabilities = []
+    def compare_vendor_data(self, desired, dumped):
+        for desired_vendor in desired:
+            self.assertTrue(any(self.compare_vendor(desired_vendor, dumped_vendor) for dumped_vendor in dumped),
+                            'the following vendor (or it\'s "superset"):\n{}\nwas not found in the dumped data:\n{}'
+                            .format(desired_vendor, dumped))
 
-        with open(os.path.join(directory, 'platform-metadata.json'), 'r') as f:
-            file_content = json.load(f)
-            platforms = file_content['platforms']['platform']
-        for platform in platforms:
-            if self.platform_name == platform['name']:
-                platform_data.append({'software-flavor': platform['software-flavor'],
-                                      'platform': platform['name'],
-                                      'os-version': platform['software-version'],
-                                      'software-version': platform['software-version'],
-                                      'feature-set': 'ALL',
-                                      'vendor': platform['vendor'],
-                                      'os': platform['os-type']})
-                if 'netconf-capabilities' in platform:
-                    netconf_version = [
-                        capability for capability in platform['netconf-capabilities'] if ':netconf:base:' in capability]
-                    netconf_capabilities = [
-                        capability for capability in platform['netconf-capabilities'] if ':capability:' in capability]
+    def compare_list(self, f, desired, dumped) -> bool:
+        for desired_datum in desired:
+            if not any(f(desired_datum, dumped_datum) for dumped_datum in dumped):
+                return False
+        return True
 
-        return platform_data, netconf_version, netconf_capabilities
+    def compare_vendor(self, desired, dumped) -> bool:
+        try:
+            if desired['name'] != dumped['name']:
+                return False
+            desired_platforms = desired['platforms']['platform']
+            dumped_platforms = dumped['platforms']['platform']
+        except KeyError:
+            return False
+        return self.compare_list(self.compare_platforms, desired_platforms, dumped_platforms)
+
+    def compare_platforms(self, desired, dumped) -> bool:
+        try:
+            if desired['name'] != dumped['name']:
+                return False
+            desired_software_versions = desired['software-versions']['software-version']
+            dumped_software_versions = dumped['software-versions']['software-version']
+        except KeyError:
+            return False
+        return self.compare_list(self.compare_software_versions, desired_software_versions, dumped_software_versions)
+
+    def compare_software_versions(self, desired, dumped) -> bool:
+        try:
+            if desired['name'] != dumped['name']:
+                return False
+            desired_software_flavors = desired['software-flavors']['software-flavor']
+            dumped_software_flavors = dumped['software-flavors']['software-flavor']
+        except KeyError:
+            return False
+        return self.compare_list(self.compare_software_flavors, desired_software_flavors, dumped_software_flavors)
+
+    def compare_software_flavors(self, desired, dumped) -> bool:
+        try:
+            if not (desired['name'] == dumped['name'] and
+                    self.compare_non_recursive(desired['protocols']['protocol'], dumped['protocols']['protocol']) and
+                    self.compare_non_recursive(desired['modules']['module'], dumped['modules']['module'])):
+                return False
+        except KeyError:
+            return False
+        return True
+
+    # used for comparing "modules" and "protocols"
+    def compare_non_recursive(self, desired: t.List[dict], dumped: t.List[dict]) -> bool:
+        """Compare a list of dicts."""
+        return self.compare_list(self.dict_contains, desired, dumped)
+
+    def dict_contains(self, sub: dict, super: dict) -> bool:
+        """Check whether the super dict contains all the keys of the sub dict with idetical values."""
+        for key in sub:
+            try:
+                if sub[key] != super[key]:
+                    return False
+            except KeyError:
+                return False
+        return True
+
+    def resource(self, path: str) -> str:
+        return os.path.join(self.resources_path, path)
 
 
 if __name__ == '__main__':
