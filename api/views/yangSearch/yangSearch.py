925f5f6737f28630a0917762e53fccaca9a69635
BohdanKonovalenko <bodiakonovalenko@gmail.com>
2022-09-29 17:17:36 +0300

    refactor: removed unnecessary part from YangSearch blueprint

--- api/views/yangSearch/yangSearch.py
+++ api/views/yangSearch/yangSearch.py
@@ -42,13 +42,7 @@ from utility.yangParser import create_context
 
 
 class YangSearch(Blueprint):
-
     LOGGER: Logger
-
-    def __init__(self, name, import_name, static_folder=None, static_url_path=None, template_folder=None,
-                 url_prefix=None, subdomain=None, url_defaults=None, root_path=None):
-        super().__init__(name, import_name, static_folder, static_url_path, template_folder, url_prefix, subdomain,
-                         url_defaults, root_path)
 
 
 bp = YangSearch('yangSearch', __name__)
